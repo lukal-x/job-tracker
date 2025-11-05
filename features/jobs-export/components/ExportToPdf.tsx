1a8a3aaf58f54ddb26ad88c0c78441d605535ab2
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2025-11-05 01:14:46 +0100

    fix export to pdf component, fix prod

--- features/jobs-export/components/ExportToPdf.tsx
+++ features/jobs-export/components/ExportToPdf.tsx
@@ -1,40 +1,46 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { Download } from "lucide-react"
-import html2pdf from 'html2pdf.js';
+"use client";
+import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
 import { sanitizeLabColors } from "@/helpers";
 import { useState } from "react";
 import Loader from "@/components/Loader";
 
-const ExportToPdf = ({ tableRef, isDisabled }: { tableRef: any, isDisabled: boolean }) => {
+const ExportToPdf = ({ tableRef, isDisabled }: { tableRef: any; isDisabled: boolean }) => {
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const handleDownloadPdf = async () => {
     setIsLoading(true);
-  
-    // Let react re-render before move to next
+
     await new Promise(resolve => setTimeout(resolve, 0));
-  
+
     sanitizeLabColors(document.body);
-  
+
     try {
-      await html2pdf().set({ filename: "Applied_Job_List.pdf" }).from(tableRef).save();
+      const html2pdf = (await import("html2pdf.js")).default;
+
+      await html2pdf()
+        .set({ filename: "Applied_Job_List.pdf" })
+        .from(tableRef)
+        .save();
     } catch (err) {
       console.error("PDF export failed", err);
     }
-  
-    location.reload();
+
+    location.reload(); 
   };
-  
-  if(isLoading){
-    return <Loader />
+
+  if (isLoading) {
+    return <Loader />;
   }
-    
+
   return (
     <div>
-        <Button disabled={isDisabled} onClick={handleDownloadPdf}><Download />Export To PDF</Button>
+      <Button disabled={isDisabled} onClick={handleDownloadPdf}>
+        <Download />
+        Export To PDF
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default ExportToPdf
+export default ExportToPdf;
